2346112844b7c51e0a3846c532c7969e50861ba2
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2025-10-05 02:34:16 +0000

    Sync LeetCode submission Runtime - 4 ms (44.65%), Memory - 17.9 MB (100.00%)

---
+++ 0002-add-two-numbers/solution.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # add first element of l1 to l2
+        # if its less than < 10, pick the right most digit and add to l3
+        # the remaning left digit should be added to the next sum
+        # at last the remaining left digit should also be added to the result
+
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            total = val1 + val2 + carry
+            carry = total // 10
+
+            # setting next node with value of digit on unit's place i.e. right side
+            current.next = ListNode(total % 10)
+            current = current.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        return dummy.next 
