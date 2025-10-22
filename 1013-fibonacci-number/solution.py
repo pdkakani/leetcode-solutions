610b4762bdc56949dc3486781fbaca231a6b3ac0
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2025-10-22 23:05:08 +0000

    Sync LeetCode submission Runtime - 31 ms (99.35%), Memory - 17.5 MB (100.00%)

---
+++ 1013-fibonacci-number/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        prev = 0
+        curr = 1
+
+        for i in range(2, n+1):
+            prev, curr = curr, prev+curr
+
+        return curr
+
+        
