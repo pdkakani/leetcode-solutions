45c7d5587d439cca3f8b9182557b27e13c690e27
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2025-11-20 04:06:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.1 MB (100.00%)

---
+++ 0020-valid-parentheses/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = deque()
+        
+        for i in s:
+    
+            if i == "(" or i == "[" or i == "{":
+                stack.append(i)
+            else:
+                if len(stack) == 0:
+                    return False
+                else:
+
+                    if i == ")" and stack[-1] == "(":
+                        stack.pop()
+                    elif i == "]" and stack[-1] == "[":
+                        stack.pop()
+                    elif i == "}" and stack[-1] == "{":
+                        stack.pop()
+                    else:
+                        return False
+        
+        return len(stack) == 0
+
