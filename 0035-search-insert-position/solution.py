14ce5b617be1cb598f9e2e80c7c120729c4bec61
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2025-11-21 01:10:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.6 MB (100.00%)

---
+++ 0035-search-insert-position/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        left = 0
+        right = n - 1
+
+        while left <= right:
+            mid = left + ((right-left) // 2)
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+
+        return left
