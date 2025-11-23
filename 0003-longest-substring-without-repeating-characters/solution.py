f99023bacd0f9a77c83573bc34e30dfc3109d16b
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2025-11-23 15:57:44 +0000

    Sync LeetCode submission Runtime - 27 ms (15.76%), Memory - 17.7 MB (100.00%)

--- 0003-longest-substring-without-repeating-characters/solution.py
+++ 0003-longest-substring-without-repeating-characters/solution.py
@@ -3,15 +3,17 @@ class Solution:
         seen = set()
         left = 0
         n = len(s)
-        longest = 0
+        res = 0
 
         for right in range(n):
             while s[right] in seen:
                 seen.remove(s[left])
                 left += 1
-            w = (right - left) + 1
-            longest = max(w, longest)
+            
+            w = right - left + 1
+            res = max(res, w)
             seen.add(s[right])
+        return res
 
-        return longest
+
         
