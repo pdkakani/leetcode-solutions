e28aa1a75b4909c4e57e462c3442bdd1d6f50147
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2025-11-25 23:13:37 +0000

    Sync LeetCode submission Runtime - 227 ms (9.78%), Memory - 17.9 MB (100.00%)

---
+++ 0424-longest-repeating-character-replacement/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
+        n = len(s)
+        max_w = 0
+        counts = [0] * 26 
+        
+        for r in range(n):
+            counts[ord(s[r]) - 65] += 1
+            while (r-l)+1 - max(counts) > k:
+                counts[ord(s[l]) - 65] -= 1
+                l+=1
+            
+            max_w = max(max_w, (r-l)+1)
+            
+        return max_w
+            
+
+        
+
+
