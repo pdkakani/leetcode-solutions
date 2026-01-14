e59f35c66c5a548df483851b87f378586f575e83
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-01-14 18:56:50 +0000

    Sync LeetCode submission Runtime - 3 ms (19.98%), Memory - 19.4 MB (61.63%)

--- 0062-unique-paths/solution.py
+++ 0062-unique-paths/solution.py
@@ -1,17 +1,20 @@
 class Solution:
-    memo = {}
+    
     def uniquePaths(self, m: int, n: int) -> int:
-        # if memo is None:
-        #     memo = {}
-        
-        if (n,m) in self.memo:
-            return self.memo.get((n,m))
-        if m == 0 or n == 0:
-            return 0
-        if m == 1 or n == 1:
-            return 1
-        
-        self.memo[(n,m)] = self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
-        return self.memo.get((n,m)) 
-        
+        table = [[0] * (n+1) for _ in range(m+1)]
+
+        table[1][1] = 1
+
+        for i in range(m+1):
+            for j in range(n+1):
+                curr = table[i][j]
+                if i + 1 <= m:
+                    table[i+1][j] += curr
+                if j + 1 <= n:
+                    table[i][j+1] += curr
+                
+
+        return table[m][n]
+
+
         
