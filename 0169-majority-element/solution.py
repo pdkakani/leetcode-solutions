233985f00b107f244715b8b9b41e4bae0bda7695
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-01-29 01:39:14 +0000

    Sync LeetCode submission Runtime - 3 ms (85.94%), Memory - 21.4 MB (14.90%)

--- 0169-majority-element/solution.py
+++ 0169-majority-element/solution.py
@@ -1,14 +1,17 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = 0
-        candidate = None
-        for i in nums:
-            if count == 0:
-                candidate = i
-            if i == candidate:
-                count +=1
-            else:
-                count -= 1
-        return candidate 
+        # count = 0
+        # candidate = None
+        # for i in nums:
+        #     if count == 0:
+        #         candidate = i
+        #     if i == candidate:
+        #         count +=1
+        #     else:
+        #         count -= 1
+        # return candidate 
+
+        counter = Counter(nums)
+        return counter.most_common()[0][0]
 
 
