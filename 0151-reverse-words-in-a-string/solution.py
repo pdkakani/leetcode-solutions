2dad98e8897ee878d44f10e2cc5852569ecf7c13
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-02-05 21:49:13 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (30.14%)

--- 0151-reverse-words-in-a-string/solution.py
+++ 0151-reverse-words-in-a-string/solution.py
@@ -1,17 +1,7 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        # first convert the string into words of array separated by space
-        words = s.split()
-        result = ""
+       inpArr = s.split()
+       inpArr.reverse() # positional reverse
 
-        left = 0
-        right = len(words) - 1
-        while left <= right:
-            temp = words[left]
-            words[left] = words[right]
-            words[right] = temp
-            left += 1
-            right -= 1
-        
-        return " ".join(words)
+       return " ".join(inpArr) 
         
