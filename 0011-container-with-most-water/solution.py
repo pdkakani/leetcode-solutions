49ddde47fc2fac639351ff4e360f32daff5376da
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-02-06 22:28:14 +0000

    Sync LeetCode submission Runtime - 56 ms (64.85%), Memory - 29.7 MB (26.62%)

--- 0011-container-with-most-water/solution.py
+++ 0011-container-with-most-water/solution.py
@@ -1,17 +1,22 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        n = len(height)
         i = 0
-        j = n - 1
-        max_area = 0
+        j = len(height) - 1 
+        ans = 0
 
+        # vertically find the two tallest heights which has max distance between them
         while i < j:
-            temp = (j - i) * min(height[i], height[j])
-            max_area = max(temp, max_area)
-            if height[i] < height[j]:
+            left = height[i]
+            right = height[j]
+
+            distance = j - i
+
+            temp_ans = distance * min(left, right)
+            ans = max(ans, temp_ans)
+
+            if left < right:
                 i += 1
             else:
                 j -= 1
-        return max_area
-
-        
+            
+        return ans        
