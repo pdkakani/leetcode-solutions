17cbe68d8cf9e1a4c676c0ede0fba36cf5ed30d1
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-02-07 01:31:53 +0000

    Sync LeetCode submission Runtime - 548 ms (78.83%), Memory - 22.4 MB (42.62%)

--- 0015-3sum/solution.py
+++ 0015-3sum/solution.py
@@ -1,30 +1,42 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        result = []
-        nums.sort()
+        # idnex should not be equal
+        # sum of values should be equal to zero
 
-        for i in range(n):
-            if nums[i] > 0:
-                break
-            if i > 0 and nums[i] == nums[i - 1]:
+        # return all the triplets matching above conditions
+        nums.sort()
+        res = []
+        n = len(nums)
+        for i in range(n - 2):
+
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            
-            low = i+1
-            high = n -1
-            while low < high:
-                curr_sum = nums[i] + nums[low] + nums[high]
-                if curr_sum == 0:
-                    result.append([nums[i], nums[low], nums[high]])
-                    low += 1
-                    high -=1
-                    while low < high and nums[low] == nums[low -1]:
-                        low+=1
-                    while low< high and nums[high] == nums[high + 1]:
-                        high-=1
-                elif curr_sum < 0:
-                    low += 1
+
+            target = -nums[i]
+            j = i +1
+            k = n - 1
+
+            while j < k:
+
+                curr_sum = nums[j] + nums[k]
+
+                if curr_sum == target:
+                    res.append([nums[i], nums[j], nums[k]])
+
+                    j += 1
+                    k -= 1
+
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    
+                    while j < k and nums[k] == nums[k+1]:
+                        k -= 1
+
+                elif curr_sum < target:
+                    j += 1
                 else:
-                    high -= 1
-        return result 
-        
+                    k -= 1
+
+        return res
+
+            
