ff0d37d5ec8b91f5250ef1c1ba41a25125d00b38
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-02-11 22:44:44 +0000

    Sync LeetCode submission Runtime - 27 ms (57.85%), Memory - 20.1 MB (29.22%)

--- 0030-substring-with-concatenation-of-all-words/solution.py
+++ 0030-substring-with-concatenation-of-all-words/solution.py
@@ -1,48 +1,38 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        if not s or not words:
-            return []
-
-        
+        num_words = len(words)
         res = []
         word_len = len(words[0])
-        num_words = len(words)
-        word_count = Counter(words)
-
+        
+        word_counter = Counter(words)
         for start in range(word_len):
             left = start
             right = start
-            current_count = Counter()
+            current_counter = Counter()
             words_used = 0
 
-            # slide window by word len
+            #slide window
             while right + word_len <= len(s):
-                #get the word at right pointer
-                word = s[right: right + word_len]
 
-                #if word is in our list
-                if word in word_count:
-                    current_count[word] += 1
+                word = s[right:right + word_len]
+                if word in word_counter:
+                    current_counter[word] += 1
                     words_used += 1
 
-                    # if we have too many of this word, shrink from left
-                    while current_count[word] > word_count[word]:
-                        left_word = s[left:left + word_len]
-                        current_count[left_word] -= 1
+                    #check for duplicates
+                    while current_counter[word] > word_counter[word]:
+                        left_word = s[left: left+word_len]
+                        current_counter[left_word] -= 1
                         words_used -= 1
                         left += word_len
                     
-                    #check if we have valid window
+                    #check if window is valid
                     if words_used == num_words:
                         res.append(left)
                 else:
-                    #word not in list, reset window
-                    current_count.clear()
+                    current_counter.clear()
                     words_used = 0
                     left = right + word_len
                 right += word_len
         return res
                 
-
-
-        
