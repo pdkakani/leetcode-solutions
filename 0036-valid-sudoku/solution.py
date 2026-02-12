89b8c0c03a157bfed5c95c91c601d1c912d61b74
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-02-12 00:47:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.5 MB (26.73%)

--- 0036-valid-sudoku/solution.py
+++ 0036-valid-sudoku/solution.py
@@ -1,35 +1,34 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # Validate the rows
+        #validate rows
         for i in range(9):
-            set1 = set()
+            valid = set()
             for j in range(9):
-                if board[i][j] in set1:
-                    return False
                 if board[i][j] != ".":
-                    set1.add(board[i][j])
-
-        # Validate the columns
-        for i in range(9):
-            set1 = set()
-            for j in range(9):
-                if board[j][i] in set1:
-                    return False
-                if board[j][i] != ".":
-                    set1.add(board[j][i])
-        # Validate the boxes
-        starts = [(0,0), (0,3), (0,6),
-                (3,0), (3,3), (3,6),
-                (6,0), (6,3), (6,6)]
-
-        for i, j in starts:
-            set1 = set()
-            for row in range(i, i+3):
-                for col in range(j, j+3):
-                    item = board[row][col]
-                    if item in set1:
+                    if board[i][j] in valid:
                         return False
-                    if item != ".":
-                        set1.add(item)
-        return True
+                    valid.add(board[i][j])
         
+        # Validate columns
+        for j in range(9):
+            valid = set()
+            for i in range(9):
+                if board[i][j] != ".":
+                    if board[i][j] in valid:
+                        return False
+                    valid.add(board[i][j])
+
+        #validate boxes
+    
+        for box_row in range(0, len(board), 3):
+            for box_col in range(0, len(board), 3):
+                box_set = set()
+                for i in range(box_row, box_row + 3):
+                    for j in range(box_col, box_col + 3):
+                        if board[i][j] not in box_set:
+                            if board[i][j] != ".":
+                                box_set.add(board[i][j])
+                        else:
+                            return False
+
+        return True
