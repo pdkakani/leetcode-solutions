d213e8d2c4800c9ff53156e6a694bb8885bc0250
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-02-14 17:17:08 +0000

    Sync LeetCode submission Runtime - 11 ms (91.11%), Memory - 19.8 MB (24.91%)

--- 0383-ransom-note/solution.py
+++ 0383-ransom-note/solution.py
@@ -1,13 +1,15 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        r = Counter(ransomNote)
-        m = Counter(magazine)
+        ransom = Counter(ransomNote)
+        mag = Counter(magazine)
 
-        for k,v in r.items():
-            if m[k] is None or m[k] < v:
+        for i, j in ransom.items():
+            if i in mag and mag[i] >= j:
+                continue
+            else:
                 return False
+        return True
 
-        return True 
 
 
         
