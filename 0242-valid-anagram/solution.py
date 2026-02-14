d36e86644371c0c5fbdc895894115087c2be6abc
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-02-14 18:34:11 +0000

    Sync LeetCode submission Runtime - 7 ms (90.44%), Memory - 19.7 MB (25.93%)

--- 0242-valid-anagram/solution.py
+++ 0242-valid-anagram/solution.py
@@ -1,11 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        count1 = {}
-        count2 = {}
-        for i in s:
-            count1[i] = count1.get(i, 0) + 1
-        for i in t:
-            count2[i] = count2.get(i, 0) + 1
+        i = Counter(s)
+        j = Counter(t)
 
-        return count1 == count2 
-        
+        return i == j
