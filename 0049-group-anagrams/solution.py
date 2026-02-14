a187a624f9fa9fd809e0f729c239714324e8e1db
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-02-14 18:43:59 +0000

    Sync LeetCode submission Runtime - 11 ms (84.66%), Memory - 22.1 MB (74.26%)

--- 0049-group-anagrams/solution.py
+++ 0049-group-anagrams/solution.py
@@ -3,6 +3,7 @@ class Solution:
         ans = defaultdict(list)
 
         for s in strs:
+            # strings are immutable so you cant sort them like list using s.sort()
             key = "".join(sorted(s))
             ans[key].append(s)
         
