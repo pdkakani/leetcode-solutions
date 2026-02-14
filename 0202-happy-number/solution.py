e6b68142fa5e2696453f04bb270c014b4bfaf445
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-02-14 20:29:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (46.57%)

--- 0202-happy-number/solution.py
+++ 0202-happy-number/solution.py
@@ -1,20 +1,16 @@
-class Solution(object):
-    def isHappy(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
-        def square_sum(n):
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+
+        while n != 1 and n not in seen:
+            seen.add(n)
+
             total = 0
             while n > 0:
                 digit = n % 10
                 total += digit ** 2
                 n //= 10
-            return total
-        seen = set()
-        while n != 1 and n not in seen:
-            seen.add(n)
-            n = square_sum(n)
+            
+            n = total
 
         return n == 1
-        
