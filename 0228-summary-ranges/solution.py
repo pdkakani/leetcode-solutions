3a7b44a58393e057d5b9f098ab18a24fbd623613
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-02-19 03:52:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (48.16%)

--- 0228-summary-ranges/solution.py
+++ 0228-summary-ranges/solution.py
@@ -1,24 +1,17 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        n = len(nums)
-        ans = []
+        res = []
         i = 0
-        
+
         while i < len(nums):
             start = nums[i]
 
-            while i < len(nums) - 1 and nums[i] + 1 == nums[i+1]:
+            while i< len(nums) - 1 and nums[i] + 1 == nums[i+1]:
                 i += 1
-            
             if start != nums[i]:
-                ans.append(str(start) + "->" + str(nums[i]))
+                res.append(f"{start}->{nums[i]}") 
             else:
-                ans.append(str(start))
+                res.append(f"{start}")
             i+=1
 
-        return ans
-
-            
-            
-
-        
+        return res
