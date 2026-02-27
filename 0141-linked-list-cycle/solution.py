e30944c7b0b3c5a97cbb3ecf59d5aeaf46688aa5
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-02-27 05:05:11 +0000

    Sync LeetCode submission Runtime - 56 ms (43.52%), Memory - 22.6 MB (70.24%)

--- 0141-linked-list-cycle/solution.py
+++ 0141-linked-list-cycle/solution.py
@@ -6,15 +6,13 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        dummy = ListNode()
-        dummy.next = head
-        slow = fast = dummy
+        fast = head
+        slow = head
 
         while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
 
-            if slow is fast:
+            if slow == fast:
                 return True
         return False
-        
