8113c5b0c3c3fb38c2df67e4d65f0bdd7fdab7bf
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-03-05 00:40:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.3 MB (91.15%)

--- 0025-reverse-nodes-in-k-group/solution.py
+++ 0025-reverse-nodes-in-k-group/solution.py
@@ -7,24 +7,26 @@ class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         dummy = ListNode(0)
         dummy.next = head
-        prev_group = dummy
+        prev_g = dummy
 
         while True:
-            kth = prev_group
+            kth = prev_g
+
             for _ in range(k):
                 kth = kth.next
                 if not kth:
                     return dummy.next
+            
+            group_n = kth.next
+            prev, curr = group_n, prev_g.next
 
-            group_next = kth.next
-            prev, curr = kth.next, prev_group.next
-
-            while curr != group_next:
-                temp = curr.next
+            while curr != group_n:
+                next_n = curr.next
                 curr.next = prev
-                prev = curr
-                curr = temp
+                prev= curr
+                curr = next_n
+            
+            temp = prev_g.next
+            prev_g.next = kth
+            prev_g = temp
 
-            temp = prev_group.next
-            prev_group.next = kth
-            prev_group = temp
