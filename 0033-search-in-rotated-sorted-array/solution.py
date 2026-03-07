a2c2f75460bc6154a93ca27c3e1466715da1d24c
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-03-07 22:23:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.5 MB (61.15%)

--- 0033-search-in-rotated-sorted-array/solution.py
+++ 0033-search-in-rotated-sorted-array/solution.py
@@ -1,39 +1,33 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        n = len(nums)
         left = 0
-        right = n - 1
-
-        while left < right:
-            mid = (left + right) // 2
-
-            if nums[mid] > nums[right]:
-                left = mid + 1
-            else:
-                right = mid
-
-        min_index = left
-        
-        if min_index == 0:
-            left = 0
-            right = n - 1
-        elif target >= nums[0] and target <= nums[min_index - 1]:
-            left = 0
-            right = min_index - 1
-        else:
-            left = min_index
-            right = n - 1
+        right = len(nums) - 1
 
         while left <= right:
             mid = (left + right) // 2
+
             if nums[mid] == target:
                 return mid
-            elif nums[mid] > target:
-                right = mid - 1
+
+            if nums[left] == nums[mid] == nums[right]:
+                left += 1
+                right -=1
+            # if true then left is sorted
+            elif nums[left] <= nums[mid]:
+                # check if the target is within the left part
+                if nums[left] <= target and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
             else:
-                left = mid + 1
+                # right part is sorted
+                # check if the target is within the right part
+                if nums[mid] < target and target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            
         return -1
-
-
+                 
 
 
