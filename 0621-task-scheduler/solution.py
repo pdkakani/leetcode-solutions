05db4aef08304bdb7358e1da5d767307027de5aa
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-03-13 19:46:59 +0000

    Sync LeetCode submission Runtime - 82 ms (66.32%), Memory - 20.9 MB (29.71%)

--- 0621-task-scheduler/solution.py
+++ 0621-task-scheduler/solution.py
@@ -1,21 +1,35 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
+        cooldown = deque() # (remaining_freq_negated, avai_at_time)
         freq = Counter(tasks)
-        heap = []
-        cooldown = deque()
-        res = 0
-        for f in freq.values():
-            heappush(heap, -f)
+        heap = [-f for f in freq.values()]
+        heapify(heap)
+
+        time = 0
 
         while heap or cooldown:
-            if heap:
-                task = -heappop(heap)
-                if task > 1:
-                    cooldown.append((task - 1, res + n + 1))
-            res += 1
+            time += 1
 
-            while cooldown and cooldown[0][1] == res:
-                task_c, freq_c = cooldown[0]
-                cooldown.popleft()
-                heappush(heap, -task_c)
-        return res
+            #release fron cooldown if read
+            while cooldown and cooldown[0][1] <= time:
+                heappush(heap, cooldown.popleft()[0])
+            
+            if heap:
+                #pick most frequent task
+                remaining = heappop(heap) + 1 #+1 because freq are negated
+                if remaining < 0: #still has runs left
+                    cooldown.append((remaining, time + n+1))
+            else:
+                #nothing available
+                #jump time to when next available
+                time = cooldown[0][1] - 1
+        
+        return time
+            
+
+
+            
+
+        
+        
+        
