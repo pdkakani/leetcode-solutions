f5f8d8e5653c56a428130431b8fd5913d1aad477
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-03-18 03:38:36 +0000

    Sync LeetCode submission Runtime - 3 ms (75.12%), Memory - 22.1 MB (77.46%)

--- 0543-diameter-of-binary-tree/solution.py
+++ 0543-diameter-of-binary-tree/solution.py
@@ -6,16 +6,28 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        self.diam = 0
-        def compute_h(node):
+        # self.diam = 0
+        # def compute_h(node):
+        #     if not node:
+        #         return 0
+            
+        #     l_h = compute_h(node.left)
+        #     r_h = compute_h(node.right)
+
+        #     self.diam = max(self.diam, l_h+r_h)
+
+        #     return 1 + max(l_h, r_h)
+        # compute_h(root)
+        # return self.diam
+        maxi = 0
+        def compute(node):
+            nonlocal maxi
             if not node:
                 return 0
-            
-            l_h = compute_h(node.left)
-            r_h = compute_h(node.right)
+            left = compute(node.left)
+            right = compute(node.right)
 
-            self.diam = max(self.diam, l_h+r_h)
-
-            return 1 + max(l_h, r_h)
-        compute_h(root)
-        return self.diam
+            maxi = max(maxi, left + right)
+            return 1 + max(left, right)
+        compute(root)
+        return maxi
