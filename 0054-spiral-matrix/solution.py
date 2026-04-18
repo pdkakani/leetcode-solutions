ef267bfb78158724f703e77eff9924bc4d529391
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-04-18 01:13:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (78.22%)

--- 0054-spiral-matrix/solution.py
+++ 0054-spiral-matrix/solution.py
@@ -1,43 +1,61 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         
-        row_len = len(matrix)
-        col_len = len(matrix[0])
+        # row_len = len(matrix)
+        # col_len = len(matrix[0])
 
-        top = 0
-        bottom = row_len -1
-        left = 0
-        right = col_len - 1
-        result = []
+        # top = 0
+        # bottom = row_len -1
+        # left = 0
+        # right = col_len - 1
+        # result = []
 
-        while top <= bottom and left <= right:
+        # while top <= bottom and left <= right:
 
-            # move left along top
-            for col in range(left, right + 1):
-                result.append(matrix[top][col])
-            top += 1
-
-
-            # move down alogn right
-            for row in range(top, bottom + 1):
-                result.append(matrix[row][right])
-            right -= 1
+        #     # move left along top
+        #     for col in range(left, right + 1):
+        #         result.append(matrix[top][col])
+        #     top += 1
 
 
-            # move left along bottom
-            if top <= bottom:
-                for col in range(right, left - 1, -1):
-                    result.append(matrix[bottom][col])
-                bottom -= 1
+        #     # move down alogn right
+        #     for row in range(top, bottom + 1):
+        #         result.append(matrix[row][right])
+        #     right -= 1
+
+
+        #     # move left along bottom
+        #     if top <= bottom:
+        #         for col in range(right, left - 1, -1):
+        #             result.append(matrix[bottom][col])
+        #         bottom -= 1
             
 
-            # move top along left
-            if left <= right:
-                for row in range(bottom, top - 1, -1):
-                    result.append(matrix[row][left])
-                left += 1
+        #     # move top along left
+        #     if left <= right:
+        #         for row in range(bottom, top - 1, -1):
+        #             result.append(matrix[row][left])
+        #         left += 1
             
-        return result
+        # return result
+
+        res = []
+
+        while matrix:
+            res += matrix.pop(0)
+
+            if matrix and matrix[0]:
+                for row in matrix:
+                    res.append(row.pop())
+            
+            if matrix:
+                res += matrix.pop()[::-1]
+            
+            if matrix and matrix[0]:
+                for row in matrix[::-1]:
+                    res.append(row.pop(0))
+        
+        return res
 
             
 
