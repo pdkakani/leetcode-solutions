5d38cd2fb7b72d166b5cd51c416370b07e4375dd
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-05-11 20:00:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.4 MB (61.34%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -2,8 +2,8 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         h = {}
 
-        for i in range(len(nums)):
-            h[nums[i]] = i
+        for i, v in enumerate(nums):
+            h[v] = i
 
         for i in range(len(nums)):
             temp = target - nums[i]
