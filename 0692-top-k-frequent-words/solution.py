f04b8e78dd9ccba19271b1ad17e2af14a7afd830
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-06-04 00:34:43 +0000

    Sync LeetCode submission Runtime - 3 ms (55.57%), Memory - 19.6 MB (19.28%)

--- 0692-top-k-frequent-words/solution.py
+++ 0692-top-k-frequent-words/solution.py
@@ -1,5 +1,8 @@
+from collections import Counter
+import heapq
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         count = Counter(words)
-        return heapq.nsmallest(k, count.keys(), key = lambda w: (-count[w], w))
+
+        return heapq.nsmallest(k, count.keys(), key = lambda x: (-count[x], x))
         
