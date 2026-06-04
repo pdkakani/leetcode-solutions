e2161fe0d8126562bcd37db7f21021de83c481e9
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-06-04 19:47:27 +0000

    Sync LeetCode submission Runtime - 227 ms (42.56%), Memory - 24 MB (11.06%)

--- 1056-capacity-to-ship-packages-within-d-days/solution.py
+++ 1056-capacity-to-ship-packages-within-d-days/solution.py
@@ -2,26 +2,26 @@ class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
         low = max(weights)
         high = sum(weights)
-        print(low, high)
-        ans = high
 
-        def is_minimum(capacity):
+        def check(cap):
             days_needed = 1
-            curr_load = 0
-            for weight in weights:
-                if curr_load + weight > capacity:
+            curr_weight = 0
+            for w in weights:
+
+                # if the current weight plus the new wieght exceeeds above cap that means ship is full, increase the days needed to send that ship by one
+                if curr_weight + w > cap:
                     days_needed += 1
-                    curr_load = 0
-                curr_load += weight
+                    curr_weight = 0
+                curr_weight += w
             return days_needed <= days
         
-        while low <= high:
+        while low < high:
             mid = (low + high) // 2
-            if is_minimum(mid):
-                ans = mid
-                high = mid - 1
+            if check(mid):
+                high = mid
             else:
                 low = mid + 1
-        return ans
+            
+        return low
 
         
