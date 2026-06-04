e44c7bfbdaaf244c4963cc92ba3796a89ccac8e6
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-06-04 05:28:53 +0000

    Sync LeetCode submission Runtime - 229 ms (92.54%), Memory - 21.5 MB (72.99%)

--- 0200-number-of-islands/solution.py
+++ 0200-number-of-islands/solution.py
@@ -1,28 +1,38 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         count = 0
-        if not grid and not grid[0]:
-            return grid
-
+        if not grid or not grid[0]:
+            return 0
         rows = len(grid)
         cols = len(grid[0])
 
         def dfs(r, c):
-            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] != "1":
+            # check bounds
+            if r < 0 or r >= rows:
+                return
+            if c < 0 or c >= cols:
                 return
 
-            grid[r][c] = 0
-            dfs(r-1, c)
-            dfs(r, c-1)
-            dfs(r+1, c)
-            dfs(r, c+1)
+            # check if its water cell
+            if grid[r][c] != '1':
+                return
+            
+            # sink the land cell
+            grid[r][c] = '0'
+
+            # check the nieghbours
+            dfs(r - 1, c) # up
+            dfs(r, c - 1) # left
+            dfs(r, c + 1) # right
+            dfs(r + 1, c) # down
         
+        # traverse from each cell
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j] == "1":
-                    count += 1
-                    dfs(i, j)
-
+                # found island
+                if grid[i][j] == '1':
+                    dfs(i,j) # sink the whole island
+                    count += 1 # count it once
+        
         return count
-
         
