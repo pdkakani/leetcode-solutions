595723309c1cb22220cc76f88ee42518bbfd15e7
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-06-04 06:02:33 +0000

    Sync LeetCode submission Runtime - 161 ms (81.62%), Memory - 20.8 MB (12.09%)

--- 0907-koko-eating-bananas/solution.py
+++ 0907-koko-eating-bananas/solution.py
@@ -1,19 +1,18 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        low = 1
-        high = max(piles)
-        ans = 1
 
-        def k_works(speed):
+        def check(speed):
             total_hrs = sum(ceil(pile/speed) for pile in piles)
             return total_hrs <= h
+        low = 1
+        high = max(piles)
 
-        while low <= high:
+        while low < high:
             mid = (low + high) // 2
-            if k_works(mid):
-                ans = mid
-                high = mid - 1
+            if check(mid):
+                high = mid
             else:
                 low = mid + 1
+
+        return low
         
-        return ans
