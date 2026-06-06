cb3fb0c10ef7f7b26829e2e0f517ca4663322105
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-06-06 01:22:47 +0000

    Sync LeetCode submission Runtime - 3 ms (76.53%), Memory - 19.3 MB (78.89%)

--- 1036-rotting-oranges/solution.py
+++ 1036-rotting-oranges/solution.py
@@ -1,13 +1,11 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        if not grid or not grid[0]:
-            return -1
-        
         rows = len(grid)
         cols = len(grid[0])
-        res = 0
-        queue = deque()
+        result = 0
         fresh = 0
+        queue = deque()
+
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 2:
@@ -17,15 +15,15 @@ class Solution:
         
         while queue and fresh > 0:
             for _ in range(len(queue)):
-                r, c = queue.popleft()
-                for dr, dc in [(1,0),(-1,0), (0,-1),(0,1)]:
+                r,c = queue.popleft()
+                for dr, dc in [(0,1),(-1,0),(1,0),(0,-1)]:
                     nr, nc = r + dr, c + dc
                     if 0 <= nr < rows and 0 <= nc < cols:
                         if grid[nr][nc] == 1:
                             grid[nr][nc] = 2
                             fresh -= 1
                             queue.append((nr,nc))
-            res += 1
-        
-        return res if fresh == 0 else -1
-        
+            result += 1
+        return result if fresh == 0 else -1
+
+
