f3a744003a436c71400febb4e9e48258693c5eb6
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-06-25 02:13:12 +0000

    Sync LeetCode submission Runtime - 13 ms (83.93%), Memory - 20 MB (97.64%)

--- 0055-jump-game/solution.py
+++ 0055-jump-game/solution.py
@@ -1,21 +1,11 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        maxReach = 0
+        n = len(nums)
+        goal = n - 1
 
-        for i in range(len(nums)):
-            # if current is beyond maxReach
-            if i > maxReach:
-                return False
-            
-            #update maxReach to farthest position
-            maxReach = max(maxReach, i + nums[i])
-
-            #if already reached end, early exit
-            if maxReach > len(nums) - 1:
-                return True
-            
-        return True
-
-
-
-
+        for i in range(n-1, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+        
+        return goal == 0
+        
