7b30381650a5a48ac0daa1db101674fceef52e9f
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-06-25 18:23:40 +0000

    Sync LeetCode submission Runtime - 27 ms (46.73%), Memory - 25.9 MB (65.99%)

--- 0134-gas-station/solution.py
+++ 0134-gas-station/solution.py
@@ -1,16 +1,16 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        total_gas = 0
+        start= 0
+        tank = 0
+
         n = len(gas)
-        total_tank = 0
-        current_tank = 0
-        start = 0
-
         for i in range(n):
-            net_gain = gas[i] - cost[i]
-            total_tank += net_gain
-            current_tank += net_gain
+            diff = gas[i] - cost[i]
+            total_gas += diff
+            tank += diff
 
-            if current_tank < 0:
+            if tank < 0:
                 start = i + 1
-                current_tank = 0
-        return start if total_tank >= 0 else -1
+                tank = 0
+        return start if total_gas >= 0 else -1   
