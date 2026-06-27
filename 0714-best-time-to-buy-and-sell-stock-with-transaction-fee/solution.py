e75a61739f8a0578f9ba1a7929c1cfdbcaec8587
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-06-27 17:54:56 +0000

    Sync LeetCode submission Runtime - 32 ms (98.32%), Memory - 26.2 MB (76.63%)

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/solution.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/solution.py
@@ -1,10 +1,25 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        res = 0
-        cash = 0
-        hold = -prices[0]
-        for p in prices:
-            cash = max(cash, hold + p - fee)
-            hold = max(hold, cash - p)
-        return cash
+        buy = prices[0] + fee
+        profit = 0
+
+        for p in range(1, len(prices)):
+            curr_price = prices[p]
+            if curr_price + fee < buy: # means i can buy since cheaper price found
+                buy = curr_price + fee
+            elif curr_price > buy: # means i can sell since higher price found
+                profit += curr_price - buy
+                buy = curr_price
+
+        return profit
+
+        # cash = 0 # empty handed
+        # hold = -prices[0] # the stock we hold
+        # for p in prices:
+        #     cash = max(cash, hold + p - fee) #updates on sell
+        #     hold = max(hold, cash - p) # updates on buy
+        # return cash
+
+
+
         
