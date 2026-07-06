9a9d718b962b2811089550ff4ec68832288eaa02
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-07-06 01:43:36 +0000

    Sync LeetCode submission Runtime - 15 ms (42.32%), Memory - 22 MB (65.38%)

--- 0049-group-anagrams/solution.py
+++ 0049-group-anagrams/solution.py
@@ -1,13 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = defaultdict(list)
-
+        res = defaultdict(list)
         for s in strs:
-            # strings are immutable so you cant sort them like list using s.sort()
-            key = "".join(sorted(s))
-            ans[key].append(s)
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
         
-        return list(ans.values())
-
-
-
