48410c98bc72b6b99c235bcd16c197e54b5c4a24
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-07-11 01:39:25 +0000

    Sync LeetCode submission Runtime - 391 ms (8.52%), Memory - 46.2 MB (6.91%)

--- 0239-sliding-window-maximum/solution.py
+++ 0239-sliding-window-maximum/solution.py
@@ -1,23 +1,16 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        d = deque()
+        heap = []
         res = []
 
-        for i in range(len(nums)):
-            # remove expired index from front
-            if d and d[0] <= i - k:
-                d.popleft()
+        for right in range(len(nums)):
+            heapq.heappush(heap, (-nums[right], right))
 
-            #remove smaller elements from the back
-            while d and nums[d[-1]] < nums[i]:
-                d.pop()
+            while heap[0][1] <= right - k:
+                heapq.heappop(heap)
             
-            d.append(i)
-
-            # start recording once first window is complete
-            if i >= k - 1:
-                res.append(nums[d[0]])
+            if right >= k - 1:
+                res.append(-heap[0][0])
             
         return res
-
-
+        
