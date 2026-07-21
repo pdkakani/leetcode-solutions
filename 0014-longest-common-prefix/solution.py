1a2f481f58f764bf3ee40f77d9c9a3df84d1b2ba
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-07-21 19:48:11 +0000

    Sync LeetCode submission Runtime - 3 ms (20.25%), Memory - 19.3 MB (30.66%)

--- 0014-longest-common-prefix/solution.py
+++ 0014-longest-common-prefix/solution.py
@@ -1,47 +1,16 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        # res = ""
-        # strs.sort(key= len)
+        if not strs:
+            return ""
+        prefix = strs[0]
 
-        # # temp stores first word
-        # temp = strs[0]
-
-        # # length of first word
-        # n = len(temp)
-        # flag = True
-        # # loop over n times - length of first word
-        # for i in range(n):
-            
-
-        #     # store the first letter
-        #     letter = strs[0][i]
-                
-            
-        #     # loop over the input arr
-        #     for j in range(1, len(strs)):
-                
-        #         if i < len(strs[j]):
-
-        #             # compare letter of first word with letter of every word in input arr
-        #             if letter != strs[j][i]:
-        #                 # if the letter is not same we break the loop and return ""
-        #                 flag = False
-        #                 break
-            
-        #     if flag:
-        #         res += letter
-
-        # return res 
-
-        smallest = min(strs)
-        n = len(smallest)
-        i = 0
-
-        while i < n:
-            for s in strs:
-                if s[i] != strs[0][i]:
-                    return s[:i]
-            i += 1
-
-        return strs[0][:i]  
+        def get_common_prefix(s1, s2):
+            i = 0
+            while i < min(len(s1), len(s2)) and s1[i] == s2[i]:
+                i += 1
+            return s1[:i]
+        
+        for s in strs[1:]:
+            prefix = get_common_prefix(prefix, s)
+        return prefix
         
