fbab2d501ad4acf52778e23ef18f7d8ff47ff4d9
Pushpak Kakani <55783888+pdkakani@users.noreply.github.com>
2026-09-03 00:22:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.5 MB (7.35%)

--- 0057-insert-interval/solution.py
+++ 0057-insert-interval/solution.py
@@ -1,24 +1,28 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
         ns, ne = newInterval
         n = len(intervals)
-        i = 0
-        res = []
 
-        while i < n and intervals[i][1] < ns:
+        i = 0
+      
+
+        # if the new interval in after the current interval
+        while i < n and ns > intervals[i][1]:
             res.append(intervals[i])
             i += 1
 
-        while i < n and intervals[i][0] <= ne and intervals[i][1] >= ns:
+        # if the new interval overlaps the current interval
+        while i < n and ne >= intervals[i][0] and ns <= intervals[i][1]:
             ns = min(intervals[i][0], ns)
             ne = max(intervals[i][1], ne)
-            i+=1
-        res.append([ns,ne])
-        
-        while i < n and intervals[i][0] > ne:
+            i += 1
+        res.append([ns, ne])
+
+        # if the new intervals in before the current interval
+        while i < n and ne < intervals[i][0]:
             res.append(intervals[i])
             i += 1
-        
         return res
 
         
